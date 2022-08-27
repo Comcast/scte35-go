8b8c5536eda491018bc5340a1ca6fc7abf207cad
Adrian <52701496+futzu@users.noreply.github.com>
2022-08-27 19:07:25 +0000

    added tests for test/test3.ts

--- pkg/scte35/stream_test.go
+++ pkg/scte35/stream_test.go
@@ -37,15 +37,19 @@ func TestStreamSCTE35(t *testing.T) {
 		want []byte
 		arg  string
 	}{
-		
+
 		{
 			name: "Stream SCTE35 Test test/test1.ts",
 			want: []byte(`{"encryptedPacket":{},"protocolVersion":0,"ptsAdjustment":0,"sapType":3,"spliceCommand":{"type":5,"program":{"spliceTime":{"ptsTime":3438281293}},"spliceEventId":94,"spliceEventCancelIndicator":false,"spliceImmediateFlag":false,"outOfNetworkIndicator":false},"spliceDescriptors":[{"type":1,"preroll":177,"chars":"121#"}],"tier":4095}`),
 			arg:  "test/test1.ts"},
-        {
+		{
 			name: "Stream SCTE35 Test test/test2.ts",
 			want: []byte(`{"encryptedPacket":{},"protocolVersion":0,"ptsAdjustment":183265,"sapType":3,"spliceCommand":{"type":5,"program":{"spliceTime":{}},"spliceEventId":6111,"spliceEventCancelIndicator":false,"spliceImmediateFlag":true,"outOfNetworkIndicator":false},"tier":4095}`),
 			arg:  "test/test2.ts"},
+		{
+			name: "Stream SCTE35 Test test/test3.ts",
+			want: []byte(`{"encryptedPacket":{"cwIndex":255},"protocolVersion":0,"ptsAdjustment":0,"sapType":3,"spliceCommand":{"type":6,"spliceTime":{"ptsTime":8098574552}},"spliceDescriptors":[{"type":2,"deliveryRestrictions":{"archiveAllowedFlag":true,"webDeliveryAllowedFlag":true,"noRegionalBlackoutFlag":true,"deviceRestrictions":3},"segmentationUpids":[{"segmentationUpidType":8,"formatIdentifier":null,"format":"text","value":"749038837"}],"components":null,"segmentationEventId":1207959560,"segmentationEventCancelIndicator":false,"segmentationDuration":null,"segmentationTypeId":23,"segmentNum":0,"segmentsExpected":0,"subSegmentNum":null,"subSegmentsExpected":null}],"tier":4095}`),
+			arg:  "test/test3.ts"},
 	}
 
 	for _, c := range cases {
@@ -75,6 +79,11 @@ func TestPacketData(t *testing.T) {
 			name: "Stream Packet Data Test test/test2.ts",
 			want: PacketData{PacketNumber: 573, PID: 258, Program: 1, PCR: 595.9, PTS: 95443.717677},
 			arg:  "test/test2.ts"},
+
+		{
+			name: "Stream Packet Data Test test/test3.ts",
+			want: PacketData{PacketNumber: 1672, PID: 309, Program: 1, PCR: 89975.5132, PTS: 89975.521522},
+			arg:  "test/test3.ts"},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
