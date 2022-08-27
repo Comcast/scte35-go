bba8ab0f8cf201849162634c96d75523dcb95254
Adrian <52701496+futzu@users.noreply.github.com>
2022-08-27 21:12:02 +0000

    Added check for Stream without Cues

--- pkg/scte35/stream_test.go
+++ pkg/scte35/stream_test.go
@@ -28,9 +28,11 @@ func MakeStream(arg string) Stream {
 	var strm Stream
 	strm.Silent = true
 	strm.Decode(arg)
+
 	return strm
 }
 
+// test for SCTE35 data in MPEGTS files
 func TestStreamSCTE35(t *testing.T) {
 	cases := []struct {
 		name string
@@ -39,15 +41,15 @@ func TestStreamSCTE35(t *testing.T) {
 	}{
 
 		{
-			name: "Stream SCTE35 Test test/test1.ts",
+			name: "Stream SCTE35 Test Splice Insert w/ DTMF Descriptor : test/test1.ts",
 			want: []byte(`{"encryptedPacket":{},"protocolVersion":0,"ptsAdjustment":0,"sapType":3,"spliceCommand":{"type":5,"program":{"spliceTime":{"ptsTime":3438281293}},"spliceEventId":94,"spliceEventCancelIndicator":false,"spliceImmediateFlag":false,"outOfNetworkIndicator":false},"spliceDescriptors":[{"type":1,"preroll":177,"chars":"121#"}],"tier":4095}`),
 			arg:  "test/test1.ts"},
 		{
-			name: "Stream SCTE35 Test test/test2.ts",
+			name: "Stream SCTE35 Test Splice Insert: test/test2.ts",
 			want: []byte(`{"encryptedPacket":{},"protocolVersion":0,"ptsAdjustment":183265,"sapType":3,"spliceCommand":{"type":5,"program":{"spliceTime":{}},"spliceEventId":6111,"spliceEventCancelIndicator":false,"spliceImmediateFlag":true,"outOfNetworkIndicator":false},"tier":4095}`),
 			arg:  "test/test2.ts"},
 		{
-			name: "Stream SCTE35 Test test/test3.ts",
+			name: "Stream SCTE35 Test Time Signal w/ Segmentation Descriptor : test/test3.ts",
 			want: []byte(`{"encryptedPacket":{"cwIndex":255},"protocolVersion":0,"ptsAdjustment":0,"sapType":3,"spliceCommand":{"type":6,"spliceTime":{"ptsTime":8098574552}},"spliceDescriptors":[{"type":2,"deliveryRestrictions":{"archiveAllowedFlag":true,"webDeliveryAllowedFlag":true,"noRegionalBlackoutFlag":true,"deviceRestrictions":3},"segmentationUpids":[{"segmentationUpidType":8,"formatIdentifier":null,"format":"text","value":"749038837"}],"components":null,"segmentationEventId":1207959560,"segmentationEventCancelIndicator":false,"segmentationDuration":null,"segmentationTypeId":23,"segmentNum":0,"segmentsExpected":0,"subSegmentNum":null,"subSegmentsExpected":null}],"tier":4095}`),
 			arg:  "test/test3.ts"},
 	}
@@ -55,11 +57,15 @@ func TestStreamSCTE35(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			strm := MakeStream(c.arg)
-			cue := &strm.Cues[0]
-			got, _ := json.Marshal(cue)
-			out, _ := AreEqualJSON(c.want, got)
-			if !out {
-				t.Errorf("Wanted:\n %s\nGot: %s", c.want, got)
+			if len(strm.Cues) > 0 {
+				cue := &strm.Cues[0]
+				got, _ := json.Marshal(cue)
+				out, _ := AreEqualJSON(c.want, got)
+				if !out {
+					t.Errorf("Wanted:\n %s\nGot: %s", c.want, got)
+				}
+			} else {
+				t.Errorf("No Cues in Stream %s", c.arg)
 			}
 		})
 	}
@@ -72,27 +78,30 @@ func TestPacketData(t *testing.T) {
 		arg  string
 	}{
 		{
-			name: "Stream Packet Data Test test/test1.ts",
+			name: "Stream Packet Data Test:  test/test1.ts",
 			want: PacketData{PacketNumber: 2283, PID: 515, Program: 51, PCR: 38199.918911, PTS: 38199.872111},
 			arg:  "test/test1.ts"},
 		{
-			name: "Stream Packet Data Test test/test2.ts",
+			name: "Stream Packet Data Test:  test/test2.ts",
 			want: PacketData{PacketNumber: 573, PID: 258, Program: 1, PCR: 595.9, PTS: 95443.717677},
 			arg:  "test/test2.ts"},
 
 		{
-			name: "Stream Packet Data Test test/test3.ts",
+			name: "Stream Packet Data Test:  test/test3.ts",
 			want: PacketData{PacketNumber: 1672, PID: 309, Program: 1, PCR: 89975.5132, PTS: 89975.521522},
 			arg:  "test/test3.ts"},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			strm := MakeStream(c.arg)
-			got := strm.Cues[0].PacketData
-			if got != c.want {
-				t.Errorf("Wanted:\n %v\nGot: %v", c.want, got)
+			if len(strm.Cues) > 0 {
+				got := strm.Cues[0].PacketData
+				if got != c.want {
+					t.Errorf("Wanted:\n %v\nGot: %v", c.want, got)
+				}
+			} else {
+				t.Errorf("No Cues in Stream %s", c.arg)
 			}
-
 		})
 	}
 }
