ea9aae63f7cb3317d546c4f0c8d749f350f2867a
Adrian <52701496+futzu@users.noreply.github.com>
2022-08-28 15:31:15 +0000

    put constants back

--- pkg/scte35/segmentation_descriptor.go
+++ pkg/scte35/segmentation_descriptor.go
@@ -28,6 +28,137 @@ const (
 	// SegmentationDescriptorTag is the splice_descriptor_tag for
 	// segmentation_descriptor
 	SegmentationDescriptorTag = 0x02
+	// SegmentationTypeNotIndicated is the segmentation_type_id for Not Indicated.
+	SegmentationTypeNotIndicated = 0x00
+	// SegmentationTypeContentIdentification is the segmentation_type_id for
+	// Content Identification.
+	SegmentationTypeContentIdentification = 0x01
+	// SegmentationTypeProgramStart is the segmentation_type_id for Program Start.
+	SegmentationTypeProgramStart = 0x10
+	// SegmentationTypeProgramEnd is the segmentation_type_id for Program End.
+	SegmentationTypeProgramEnd = 0x11
+	// SegmentationTypeProgramEarlyTermination is the segmentation_type_id for
+	// Program Early Termination.
+	SegmentationTypeProgramEarlyTermination = 0x12
+	// SegmentationTypeProgramBreakaway is the segmentation_type_id for
+	// Program Breakaway.
+	SegmentationTypeProgramBreakaway = 0x13
+	// SegmentationTypeProgramResumption is the segmentation_type_id for Program
+	// Resumption.
+	SegmentationTypeProgramResumption = 0x14
+	// SegmentationTypeProgramRunoverPlanned is the segmentation_type_id for
+	// Program Runover Planned.
+	SegmentationTypeProgramRunoverPlanned = 0x15
+	// SegmentationTypeProgramRunoverUnplanned is the segmentation_type_id for
+	// Program Runover Unplanned.
+	SegmentationTypeProgramRunoverUnplanned = 0x16
+	// SegmentationTypeProgramOverlapStart is the segmentation_type_id for Program
+	// Overlap Start.
+	SegmentationTypeProgramOverlapStart = 0x17
+	// SegmentationTypeProgramBlackoutOverride is the segmentation_type_id for
+	// Program Blackout Override.
+	SegmentationTypeProgramBlackoutOverride = 0x18
+	// SegmentationTypeProgramStartInProgress is the segmentation_type_id for
+	// Program Start - In Progress.
+	SegmentationTypeProgramStartInProgress = 0x19
+	// SegmentationTypeChapterStart is the segmentation_type_id for Chapter Start.
+	SegmentationTypeChapterStart = 0x20
+	// SegmentationTypeChapterEnd is the segmentation_type_id for Chapter End.
+	SegmentationTypeChapterEnd = 0x21
+	// SegmentationTypeBreakStart is the segmentation_type_id for Break Start.
+	// Added in ANSI/SCTE 2017.
+	SegmentationTypeBreakStart = 0x22
+	// SegmentationTypeBreakEnd is the segmentation_type_id for Break End.
+	// Added in ANSI/SCTE 2017.
+	SegmentationTypeBreakEnd = 0x23
+	// SegmentationTypeOpeningCreditStart is the segmentation_type_id for
+	// Opening Credit Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeOpeningCreditStart = 0x24
+	// SegmentationTypeOpeningCreditEnd is the segmentation_type_id for
+	// Opening Credit End. Added in ANSI/SCTE 2020.
+	SegmentationTypeOpeningCreditEnd = 0x25
+	// SegmentationTypeClosingCreditStart is the segmentation_type_id for
+	// Closing Credit Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeClosingCreditStart = 0x26
+	// SegmentationTypeClosingCreditEnd is the segmentation_type_id for
+	// Closing Credit End. Added in ANSI/SCTE 2020.
+	SegmentationTypeClosingCreditEnd = 0x27
+	// SegmentationTypeProviderAdStart is the segmentation_type_id for Provider
+	// Ad Start.
+	SegmentationTypeProviderAdStart = 0x30
+	// SegmentationTypeProviderAdEnd is the segmentation_type_id for Provider Ad
+	// End.
+	SegmentationTypeProviderAdEnd = 0x31
+	// SegmentationTypeDistributorAdStart is the segmentation_type_id for
+	// Distributor Ad Start.
+	SegmentationTypeDistributorAdStart = 0x32
+	// SegmentationTypeDistributorAdEnd is the segmentation_type_id for
+	// Distributor Ad End.
+	SegmentationTypeDistributorAdEnd = 0x33
+	// SegmentationTypeProviderPOStart is the segmentation_type_id for Provider
+	// PO Start.
+	SegmentationTypeProviderPOStart = 0x34
+	// SegmentationTypeProviderPOEnd is the segmentation_type_id for Provider PO
+	// End.
+	SegmentationTypeProviderPOEnd = 0x35
+	// SegmentationTypeDistributorPOStart is the segmentation_type_id for
+	// Distributor PO Start.
+	SegmentationTypeDistributorPOStart = 0x36
+	// SegmentationTypeDistributorPOEnd is the segmentation_type_id for
+	// Distributor PO End.
+	SegmentationTypeDistributorPOEnd = 0x37
+	// SegmentationTypeProviderOverlayPOStart is the segmentation_type_id for
+	// Provider Overlay Placement Opportunity Start.
+	SegmentationTypeProviderOverlayPOStart = 0x38
+	// SegmentationTypeProviderOverlayPOEnd is the segmentation_type_id for
+	// Provider Overlay Placement Opportunity End.
+	SegmentationTypeProviderOverlayPOEnd = 0x39
+	// SegmentationTypeDistributorOverlayPOStart is the segmentation_type_id for
+	// Distributor Overlay Placement Opportunity Start.
+	SegmentationTypeDistributorOverlayPOStart = 0x3a
+	// SegmentationTypeDistributorOverlayPOEnd is the segmentation_type_id for
+	// Distributor Overlay Placement Opportunity End.
+	SegmentationTypeDistributorOverlayPOEnd = 0x3b
+	// SegmentationTypeProviderPromoStart is the segmentation_type_id for
+	// Provider Promo Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeProviderPromoStart = 0x3c
+	// SegmentationTypeProviderPromoEnd is the segmentation_type_id for
+	// Provider Promo End. Added in ANSI/SCTE 2020.
+	SegmentationTypeProviderPromoEnd = 0x3d
+	// SegmentationTypeDistributorPromoStart is the segmentation_type_id for
+	// Distributor Promo Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeDistributorPromoStart = 0x3e
+	// SegmentationTypeDistributorPromoEnd is the segmentation_type_id for
+	// Distributor Promo End. Added in ANSI/SCTE 2020.
+	SegmentationTypeDistributorPromoEnd = 0x3f
+	// SegmentationTypeUnscheduledEventStart is the segmentation_type_id for
+	// Unscheduled Event Start.
+	SegmentationTypeUnscheduledEventStart = 0x40
+	// SegmentationTypeUnscheduledEventEnd is the segmentation_type_id for
+	// Unscheduled Event End.
+	SegmentationTypeUnscheduledEventEnd = 0x41
+	// SegmentationTypeAltConOppStart is the segmentation_type_id for
+	// Alternate Content Opportunity Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeAltConOppStart = 0x42
+	// SegmentationTypeAltConOppEnd is the segmentation_type_id for
+	// Alternate Content Opportunity End. Added in ANSI/SCTE 2020.
+	SegmentationTypeAltConOppEnd = 0x43
+	// SegmentationTypeProviderAdBlockStart is the segmentation_type_id for
+	// Provider Ad Block Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeProviderAdBlockStart = 0x44
+	// SegmentationTypeProviderAdBlockEnd is the segmentation_type_id for
+	// Provider Ad Block End. Added in ANSI/SCTE 2020.
+	SegmentationTypeProviderAdBlockEnd = 0x45
+	// SegmentationTypeDistributorAdBlockStart is the segmentation_type_id for
+	// Distributor Ad Block Start. Added in ANSI/SCTE 2020.
+	SegmentationTypeDistributorAdBlockStart = 0x46
+	// SegmentationTypeDistributorAdBlockEnd is the segmentation_type_id for
+	// Distributor Ad Block End. Added in ANSI/SCTE 2020.
+	SegmentationTypeDistributorAdBlockEnd = 0x47
+	// SegmentationTypeNetworkStart is the segmentation_type_id for Network Start.
+	SegmentationTypeNetworkStart = 0x50
+	// SegmentationTypeNetworkEnd is the segmentation_type_id for Network End.
+	SegmentationTypeNetworkEnd = 0x51
 )
 
 // SegmentTypeIDs that get sub segments
